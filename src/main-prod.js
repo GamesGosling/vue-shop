fd66507d38465c0fcb2fb7d9a42e0fa1cdbeeac8
123 <123>
2020-02-08 12:15:03 +0800

    vue-shop完成开发

---
+++ src/main-prod.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import './assets/css/global.css'
+import './assets/fonts/iconfont.css'
+import axios from 'axios'
+import TreeTable from 'vue-table-with-tree-grid'
+import VueQuillEditor from 'vue-quill-editor'
+import NProgress from 'nprogress'
+// 配置请求的根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 在request 拦截器中展示进度条 axios 请求拦截器添加token, 保证拥有获取数据的权限
+axios.interceptors.request.use(config => {
+  NProgress.start()
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
+// 在response 拦截器中隐藏进度条
+axios.interceptors.response.use(config => {
+  NProgress.done()
+  return config
+})
+Vue.prototype.$http = axios
+
+Vue.component('tree-table', TreeTable)
+
+Vue.filter('dateFormat', function (originVal) {
+  const dt = new Date(originVal)
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
+
+Vue.use(VueQuillEditor)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,29 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Login from '../components/login.vue'
-import Home from '../components/home.vue'
-import Welcome from '../components/welcome.vue'
-import Users from '../components/user/users.vue'
-import Rights from '../components/power/rights.vue'
-import Roles from '../components/power/roles.vue'
-import Cate from '../components/goods/cate.vue'
-import Params from '../components/goods/params.vue'
-import GoodsList from '../components/goods/list.vue'
-import Add from '../components/goods/add.vue'
-import Order from '../components/order/order.vue'
+// import Login from '../components/login.vue'
+// import Home from '../components/home.vue'
+// import Welcome from '../components/welcome.vue'
+const Login = () => import(/* webpackChunkName: "login_home_welcome" */ '../components/login.vue')
+const Home = () => import(/* webpackChunkName: "login_home_welcome" */ '../components/home.vue')
+const Welcome = () => import(/* webpackChunkName: "login_home_welcome" */ '../components/welcome.vue')
+// import Users from '../components/user/users.vue'
+// import Rights from '../components/power/rights.vue'
+// import Roles from '../components/power/roles.vue'
+const Users = () => import(/* webpackChunkName: "users_rights_roles" */ '../components/user/users.vue')
+const Rights = () => import(/* webpackChunkName: "users_rights_roles" */ '../components/power/rights.vue')
+const Roles = () => import(/* webpackChunkName: "users_rights_roles" */ '../components/power/roles.vue')
+// import Cate from '../components/goods/cate.vue'
+// import Params from '../components/goods/params.vue'
+// import GoodsList from '../components/goods/list.vue'
+const Cate = () => import(/* webpackChunkName: "cate_params_goodsList" */ '../components/goods/cate.vue')
+const Params = () => import(/* webpackChunkName: "cate_params_goodsList" */ '../components/goods/params.vue')
+const GoodsList = () => import(/* webpackChunkName: "cate_params_goodsList" */ '../components/goods/list.vue')
+// import Add from '../components/goods/add.vue'
+// import Order from '../components/order/order.vue'
+// import Report from '../components/report/report.vue'
+const Add = () => import(/* webpackChunkName: "add_order_report" */ '../components/goods/add.vue')
+const Order = () => import(/* webpackChunkName: "add_order_report" */ '../components/order/order.vue')
+const Report = () => import(/* webpackChunkName: "add_order_report" */ '../components/report/report.vue')
 
 Vue.use(VueRouter)
 
@@ -29,7 +42,8 @@ const routes = [
       { path: '/params', component: Params },
       { path: '/goods', component: GoodsList },
       { path: '/goods/add', component: Add },
-      { path: '/orders', component: Order }
+      { path: '/orders', component: Order },
+      { path: '/reports', component: Report }
     ] }
 ]
 
